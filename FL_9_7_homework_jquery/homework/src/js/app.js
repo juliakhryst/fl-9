de1283921bde3ae1e266794aa15816ae4fd2c90b
Julia Khryst <juleczkacezar@gmail.com>
2018-10-16 23:59:14 +0200

    HW 7 fixes

--- FL_9_7_homework_jquery/homework/src/js/app.js
+++ FL_9_7_homework_jquery/homework/src/js/app.js
@@ -1,45 +1,124 @@
 const dataUrl = 'data/media.json';
-const itemsNumber = 12;
-const postsArray = [];
+const basicPostsAmmount = 12;
+const additionalPostAmmount = 6;
+let postsArray = [];
+let userData = {};
+let lastIndexPost;
+let currentModalPostId;
 
-function layout() {
+function initPage() {
     $.getJSON(dataUrl, (data) => {
-        const items = [];
-        const rowItems = [];
-        $.each(data.media, (index, post) => {
-            let overlay = `<div class="post-overlay">
-                <i class="icon icon-like"></i><span class="meta-text">${post.edge_liked_by.count}</span>
-                <i class="icon icon-comment"></i><span class="meta-text">${post.edge_media_to_comment.count}</span>
-            </div>`;
-            let img = `<div class="image" style="background-image: url(${post.display_url})"></div>`;
-            let item = `<div class="post-item" data-id='${post.id}'>${img}${overlay}</div>`;
-            items.push(item);
-            postsArray.push(post);
-            if ((index + 1) % 3 === 0) {
-                rowItems.push(`<div class="row">${items.join('')}</div>`);
-                items.length = 0;
-            }
-        });
-
-        $(rowItems.join('')).appendTo('#container');
-
+        postsArray = data.media;
+        userData.profile_pic_url = data.profile_pic_url;
+        userData.username = data.username;
+        const initialPosts = []
+        for (let i = 0; i < basicPostsAmmount; i++) {
+            initialPosts.push(postsArray[i]);
+        }
+        lastIndexPost = basicPostsAmmount;
+        renderPostGrid(initialPosts);
     });
 }
 
 $('#container').on('click', '.post-item', (e) => {
-    let postId = $('.post-item').attr('data-id');
-    console.log(postId);
+    let postId = $(e.currentTarget).attr('data-id');
     let post = $.grep(postsArray, (obj) => obj.id === postId)[0];
-    console.log(post)
+    currentModalPostId = postId;
     renderPostModal(post);
+
+    if (currentModalPostId < 1) {
+        $('.prev').hide();
+    } else {
+        $('.prev').show();
+    }
+
+    if (currentModalPostId === postsArray.length - 1) {
+        $('.next').hide();
+    } else {
+        $('.next').show();
+    }
 });
+
+$('.modal').on('click', () => {
+    $('.modal').hide();
+});
+
+$(document).keyup((e) => {
+    if (e.keyCode === 27) {
+        $('.modal').hide();
+    }
+});
+
+$('.more-link').on('click', () => {
+    const nextPostsArray = postsArray.slice(lastIndexPost + 1, lastIndexPost + additionalPostAmmount + 1);
+    renderPostGrid(nextPostsArray);
+    lastIndexPost += additionalPostAmmount;
+
+    $('.more-link').hide();
+});
+
+$('.prev').on('click', (e) => {
+    e.stopPropagation();
+    currentModalPostId--;
+    renderPostById(currentModalPostId);
+
+    if (currentModalPostId < 1) {
+        $('.prev').hide();
+    }
+
+    if (currentModalPostId < postsArray.length - 1) {
+        $('.next').show();
+    }
+});
+
+$('.next').on('click', (e) => {
+    e.stopPropagation();
+    currentModalPostId++;
+    renderPostById(currentModalPostId);
+
+    if (currentModalPostId === postsArray.length - 1) {
+        $('.next').hide();
+    }
+
+    if (currentModalPostId > 1) {
+        $('.prev').show();
+    }
+})
+
+function renderPostGrid(postArray) {
+    const items = [];
+    const rowItems = [];
+    $.each(postArray, (index, post) => {
+        let overlay = `<div class="post-overlay">
+            <i class="icon icon-like"></i><span class="meta-text">${post.edge_liked_by.count}</span>
+            <i class="icon icon-comment"></i><span class="meta-text">${post.edge_media_to_comment.count}</span>
+        </div>`;
+        let img = `<div class="image" style="background-image: url(${post.display_url})"></div>`;
+        let item = `<div class="post-item" data-id='${post.id}'>${img}${overlay}</div>`;
+        items.push(item);
+        if ((index + 1) % 3 === 0) {
+            rowItems.push(`<div class="row">${items.join('')}</div>`);
+            items.length = 0;
+        }
+
+    });
+
+    $(rowItems.join('')).appendTo('#container');
+}
 
 function renderPostModal(postItem) {
     $('#post-modal').show();
-    $modal = $('#post-modal .modal-content');
-    $modal.find('.post-image').css('background-image', `url(${postItem.display_url}`);
-    $modal.find('.location').text(postItem.location);
-    $modal.find('.caption').text(postItem.edge_media_to_caption);
+    $('.post-image').prop('src', postItem.display_url);
+    $('.logo').prop('src', userData.profile_pic_url);
+    $('.blog-name').text(userData.username);
+    $('.location').text(postItem.location);
+    $('.caption').html(`<b>${userData.username}</b> ${postItem.edge_media_to_caption}`);
+    $('.likes-row .likes').text(postItem.edge_liked_by.count);
 }
 
-layout();
+function renderPostById(postId) {
+    let post = $.grep(postsArray, (obj) => parseInt(obj.id) === postId)[0];
+    renderPostModal(post);
+}
+
+initPage();
