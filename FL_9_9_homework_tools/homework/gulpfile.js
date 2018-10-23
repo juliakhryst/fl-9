63ef41f38bf4b4e491d9811905c3b6dff0899e17
Julia Khryst <juleczkacezar@gmail.com>
2018-10-23 23:51:03 +0300

    fixes

--- FL_9_9_homework_tools/homework/gulpfile.js
+++ FL_9_9_homework_tools/homework/gulpfile.js
@@ -4,89 +4,115 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     connect = require('gulp-connect'),
     sass = require('gulp-sass'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify-es').default,
     clean = require('gulp-clean');
     jshint = require('gulp-jshint')
     sourcemaps = require('gulp-sourcemaps'),
-    runSequence = require('gulp-run-sequence'),
+    runSequence = require('run-sequence');
+
+const jsSourceFiles = ['node_modules/moment/min/moment.min.js', 'src/js/canvasState.js', 'src/js/clock.js', 'src/js/app.js'];
+const sassSourcePath = 'src/sass/*.scss';
+const devFolder = 'dev';
+const prodFolder = 'dist';
+const jsOuptutFileName = 'app.min.js';
+const cssOuptutFileName = 'style.min.css';
 
 sass.compiler = require('node-sass');
 
 gulp.task('connect', function () {
     connect.server({
-        root: 'src',
+        root: devFolder,
         livereload: true,
         port: 8080
     });
 });
 
-
 gulp.task('watch', function () {
     gulp.watch('./src/*.html', ['html']);
-    gulp.watch('./src/sass/*.scss', ['css-source-map']);
-    gulp.watch('./src/js/*.js', ['js-source-map']);
+    gulp.watch(sassSourcePath, ['css-compile']);
+    gulp.watch('./src/*.js', ['js-compile']);
 });
 
-gulp.task('start',function(){
+gulp.task('default',function(){
     runSequence('connect', 'watch');
 })
 
 gulp.task('clean', function () {
-    return gulp.src('dev', {read: false})
+    return gulp.src(devFolder, {read: false})
         .pipe(clean());
 });
 
 gulp.task('clean-prod', function () {
-    return gulp.src('dist', {read: false})
+    return gulp.src(prodFolder, {read: false})
         .pipe(clean());
 });
 
-gulp.task('jsmin',function(){
-    gulp.src('src/js/*.js')
+
+// Compile js with source maps
+gulp.task('js-compile',function(){
+    return gulp.src(jsSourceFiles)
         .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('app.min.js'))
+        .pipe(concat(jsOuptutFileName))
         .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dev/js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(`${devFolder}/js`))
         .pipe(connect.reload());
 });
 
-gulp.task('cssmin',function() {
-    gulp.src('src/sass/*.scss')
+
+// Build prod js without sourcemaps
+gulp.task('js-build',function(){
+    return gulp.src(jsSourceFiles)
+        .pipe(concat(jsOuptutFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${devFolder}/js`));
+});
+
+// Compile sass with sourcemaps
+gulp.task('css-compile',function() {
+    return gulp.src(sassSourcePath)
         .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss())
+        .pipe(concat(cssOuptutFileName))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(`${devFolder}/css`))
+        .pipe(connect.reload());
+});
+
+// Compile prod css without sourcemaps
+gulp.task('css-prod',function() {
+    return gulp.src(sassSourcePath)
         .pipe(sass())
         .pipe(cleanCss())
-        .pipe(concat('style.min.css'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dev/css'))
-        .pipe(connect.reload());
+        .pipe(concat(cssOuptutFileName))
+        .pipe(gulp.dest(`${devFolder}/css`));
 });
 
 gulp.task('html',function() {
-    gulp.src('src/app.html')
+    return gulp.src('src/*.html')
         .pipe(concat('index.html'))
-        .pipe(gulp.dest('dev'))
+        .pipe(gulp.dest(devFolder))
         .pipe(connect.reload());
 });
 
+// Lint js
 gulp.task('check', function(){
-    return gulp.src('src/js/*.js')
+    return gulp.src('js/*.js')
         .pipe(jshint())
 })
 
 gulp.task('move-to-dist', function(){
-    return gulp.src(['dev/js/app.min.js','dev/css/*.css','dev/index.html'])
-    .pipe(gulp.dest('dist'))
+    return gulp.src(['dev/**'], { base: devFolder })
+    .pipe(gulp.dest(prodFolder))
 })
+
 //build-dev
 gulp.task('build',function(){
-    runSequence('clean', ['jsmin', 'cssmin' ,'html']);
+    runSequence('clean', ['js-compile', 'css-compile', 'html']);
 });
 
 //build-prod
 gulp.task('build-prod', function () {
-    runSequence('clean-prod',['jsmin', 'cssmin' ,'html'], 'move-to-dist');
+    runSequence(['clean-prod', 'clean'], ['js-build', 'css-prod' ,'html'], 'move-to-dist');
 });
