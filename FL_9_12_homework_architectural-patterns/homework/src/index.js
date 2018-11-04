9388aa91801b558333564959daac6d32fd8aeb2a
Julia Khryst <juleczkacezar@gmail.com>
2018-11-04 23:04:25 +0200

    HW 12

---
+++ FL_9_12_homework_architectural-patterns/homework/src/index.js
@@ -0,0 +1,16 @@
+import './style.scss';
+import {createStore} from 'redux';
+import users from './reducers/users';
+import UsersTable from './components/users-table';
+import Search from './components/search';
+import ViewControls from './components/view-controls';
+
+const store = createStore(users);
+
+const usersTable = new UsersTable(store);
+const search = new Search(store);
+const viewControls = new ViewControls(store);
+
+usersTable.init();
+search.init();
+viewControls.init();
