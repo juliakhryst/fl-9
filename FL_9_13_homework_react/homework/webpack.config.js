055ea422c34707d7f9af02e3aba84420aa8dfdb7
Julia Khryst <juleczkacezar@gmail.com>
2018-11-06 22:34:32 +0200

    linter errors fixed

--- FL_9_13_homework_react/homework/webpack.config.js
+++ FL_9_13_homework_react/homework/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpackConfig = {
@@ -10,10 +10,10 @@ const webpackConfig = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css', '.scss']
+    extensions: ['.js', '.jsx', '.css', '.scss'],
   },
   module: {
     rules: [
@@ -21,16 +21,16 @@ const webpackConfig = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.scss$/,
@@ -38,32 +38,32 @@ const webpackConfig = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
       },
       {
         test: /\.(png|jpg|gif|mp3|woff(2)?|ttf|eot|svg)$/,
         use: [
           {
             loader: 'file-loader',
-          }
-        ]
-      }
-    ]
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-    })
+      filename: '[name].css',
+    }),
   ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   },
 };
 
